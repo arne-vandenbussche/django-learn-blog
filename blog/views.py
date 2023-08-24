bba371ba23e8d93b6c9f199cf36155b141578d5b
Arne Vandenbussche <arne@arnevandenbussche.be>
2023-08-24 12:09:01 +0200

    loginmodule voorzien

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 # blog/views.py
 from django.shortcuts import render 
-from django.views.generic import ListView, DetailView, CreateView, UpdateView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
 from .models import Post
 
 class BlogListView(ListView):
@@ -23,3 +24,9 @@ class BlogUpdateView(UpdateView):
     model = Post
     template_name = "post_edit.html"
     fields = ["title", "author", "body"]
+
+
+class BlogDeleteView(DeleteView):
+    model = Post
+    template_name = "post_delete.html"
+    success_url = reverse_lazy("home")
